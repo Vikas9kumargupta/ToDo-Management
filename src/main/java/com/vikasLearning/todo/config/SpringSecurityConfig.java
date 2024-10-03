8cd9de50df740d53a0725bdba8a760b8d13f8343
Vikas Kumar Gupta <vikasgup074@gmail.com>
2024-10-03 20:56:15 +0530

    Implementing Features of Spring Security -> Basic and In-Memory Authentication

---
+++ src/main/java/com/vikasLearning/todo/config/SpringSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.vikasLearning.todo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration //it makes a class Spring Java Based Configuration and here we can define Spring beans.
+public class SpringSecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests((authorize)->{
+            authorize.anyRequest().authenticated();
+        }).httpBasic(Customizer.withDefaults());
+        return http.build();
+
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails vikas = User.builder()
+                .username("vikas").password(passwordEncoder().encode("password"))
+                .roles("USER").build();
+
+        UserDetails admin = User.builder()
+                .username("admin").password(passwordEncoder().encode("password"))
+                .roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(vikas, admin);
+    }
+}
